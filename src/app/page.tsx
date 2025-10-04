a3182931c5b8b218dbe0aa4438b1b20668981f5b
ericcchuang <eyh3@njit.edu>
2025-10-04 14:00:44 -0400

    funny customer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,25 @@ import Image from "next/image";
 import { GoogleGenAI } from '@google/genai'
 import 'dotenv/config'
 interface FormElements extends HTMLFormControlsCollection {
-    textbox: HTMLInputElement
+  textbox: HTMLInputElement
 }
 
 interface MyElement extends HTMLFormElement {
-   readonly elements: FormElements
+  readonly elements: FormElements
 }
 const GEMINI_API_KEY = process.env.NEXT_PUBLIC_GEMINI_KEY;
 
-const ai = new GoogleGenAI({apiKey: GEMINI_API_KEY});
+const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 
 export default function Home() {
   const [val, setVal] = useState("");
-    async function main(prompt: string) {
+  async function main(prompt: string) {
     const response = await ai.models.generateContent({
       model: 'gemini-2.5-flash',
       contents: prompt,
+      config: {
+        systemInstruction: "You are a customer at a grocery store checking out right now. Respond as if I were a cashier who is checking out items too slowly.",
+      },
     });
     return response.text;
   }
@@ -31,13 +34,13 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          id="textbox"
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            id="textbox"
           />
           <button type="submit">Submit</button>
-          </form>
+        </form>
         <p>{val}</p>
       </main>
     </div>
