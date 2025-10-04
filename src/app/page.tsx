5c42ade1e7985b03e0926993e34998a1d9cc95ef
Logan Kriebel <logan.kriebel@gmail.com>
2025-10-04 13:38:34 -0400

    working gemini kinda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,43 @@
+'use client';
+import React from "react";
 import Image from "next/image";
-import { GoogleGenAI } from "@google/genai";
+import { GoogleGenAI } from '@google/genai'
+import 'dotenv/config'
+interface FormElements extends HTMLFormControlsCollection {
+    textbox: HTMLInputElement
+}
 
+interface MyElement extends HTMLFormElement {
+   readonly elements: FormElements
+}
+const GEMINI_API_KEY = process.env.NEXT_PUBLIC_GEMINI_KEY;
+
+console.log(GEMINI_API_KEY);
+const ai = new GoogleGenAI({apiKey: GEMINI_API_KEY});
+
+async function main(prompt: string) {
+  const response = await ai.models.generateContent({
+    model: 'gemini-2.5-flash',
+    contents: prompt,
+  });
+  console.log(response.text);
+}
+async function handleSubmit(e: React.FormEvent<MyElement>) {
+  e.preventDefault();
+  const val = await main(e.currentTarget.elements.textbox.value);
+  console.log(val);
+};
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="textbox"
+          />
+          <button type="submit">Submit</button>
+          </form>
         <Image
           className="dark:invert"
           src="/next.svg"
